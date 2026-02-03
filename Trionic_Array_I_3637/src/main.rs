562cf4291e566631a63a4c69a8f1c88d64c4a4da
gurbbanov <gurbanov.amin2004@icloud.com>
2026-02-03 22:32:37 +0300

    3637. Trionic Array I (added new solution)

--- Trionic_Array_I_3637/src/main.rs
+++ Trionic_Array_I_3637/src/main.rs
@@ -3,6 +3,40 @@ fn main() {}
 struct Solution;
 
 impl Solution {
+    pub fn is_trionic_new(nums: Vec<i32>) -> bool {
+        let mut first_bool = false;
+        let mut second_bool = false;
+        let mut ind = 0;
+
+        for win in nums.windows(2) {
+            if win[0] == win[1] {
+                return false;
+            }
+
+            if !first_bool {
+                if win[0] < win[1] {
+                    ind += 1;
+                } else {
+                    if ind == 0 {
+                        return false;
+                    }
+
+                    first_bool = true;
+                }
+            } else if !second_bool {
+                if win[0] < win[1] {
+                    second_bool = true;
+                }
+            } else {
+                if win[0] > win[1] {
+                    return false;
+                }
+            }
+        }
+
+        first_bool && second_bool
+    }
+
     pub fn is_trionic(nums: Vec<i32>) -> bool {
         let length = nums.len();
 
